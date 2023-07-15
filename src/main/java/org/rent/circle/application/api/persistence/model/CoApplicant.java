99f1437fe377480f809e993f5dc3449a25e48f6e
matos-nelson <nmatos6@gatech.edu>
2023-07-14 22:48:03 -0500

    Create co applicant model.

---
+++ src/main/java/org/rent/circle/application/api/persistence/model/CoApplicant.java
@@ -0,0 +1,39 @@
+package org.rent.circle.application.api.persistence.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.rent.circle.application.api.persistence.BaseModel;
+
+@Entity
+@Table(name = "co_applicant")
+@Setter
+@Getter
+@ToString
+public class CoApplicant extends BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "applicant_id")
+    private Long applicantId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phone;
+}

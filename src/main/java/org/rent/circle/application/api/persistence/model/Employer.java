d024d6d4bdf2f45d01a37d7d0f49ec874408f310
matos-nelson <nmatos6@gatech.edu>
2023-07-15 11:08:17 -0500

    Created employer model.

---
+++ src/main/java/org/rent/circle/application/api/persistence/model/Employer.java
@@ -0,0 +1,52 @@
+package org.rent.circle.application.api.persistence.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.rent.circle.application.api.persistence.BaseModel;
+
+@Entity
+@Table(name = "employer")
+@Setter
+@Getter
+@ToString
+public class Employer extends BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "applicant_id")
+    private Long applicantId;
+
+    @Column(name = "address_id")
+    private Long addressId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "monthlySalary")
+    private BigDecimal monthlySalary;
+
+    @Column(name = "position_held")
+    private String positionHeld;
+
+    @Column(name = "years_worked")
+    private Byte yearsWorked;
+
+    @Column(name = "supervisor_name")
+    private String supervisorName;
+
+    @Column(name = "supervisor_email")
+    private String supervisorEmail;
+}

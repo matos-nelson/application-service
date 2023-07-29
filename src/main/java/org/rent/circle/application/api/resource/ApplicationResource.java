ffe8ad10c64feecf2c590d09232a011a159a8cf3
matos-nelson <nmatos6@gatech.edu>
2023-07-29 13:56:37 -0500

    Implemented update application status endpoint.

--- src/main/java/org/rent/circle/application/api/resource/ApplicationResource.java
+++ src/main/java/org/rent/circle/application/api/resource/ApplicationResource.java
@@ -1,14 +1,18 @@
 package org.rent.circle.application.api.resource;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.rent.circle.application.api.dto.SaveApplicationDto;
+import org.rent.circle.application.api.dto.UpdateApplicationStatusDto;
 import org.rent.circle.application.api.service.ApplicationService;
 
 @AllArgsConstructor
@@ -23,5 +27,13 @@ public class ApplicationResource {
     @POST
     public Long saveApplication(@Valid SaveApplicationDto saveApplication) {
         return applicationService.saveApplication(saveApplication);
+    }
+
+    @PATCH
+    @Path("/{id}/status")
+    public void updateApplicationStatus(
+        @NotNull @PathParam("id") Long applicationId,
+        @Valid UpdateApplicationStatusDto applicationStatus) {
+        applicationService.updateApplicationStatus(applicationId, applicationStatus.getStatus());
     }
 }

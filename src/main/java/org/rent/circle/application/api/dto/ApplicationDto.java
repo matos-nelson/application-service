1d2401234606f092d134ae0cebdaed18ede37ebe
matos-nelson <nmatos6@gatech.edu>
2023-07-30 18:43:40 -0500

    Created ApplicationDto.

---
+++ src/main/java/org/rent/circle/application/api/dto/ApplicationDto.java
@@ -0,0 +1,22 @@
+package org.rent.circle.application.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.rent.circle.application.api.enums.Status;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApplicationDto {
+
+    private Long id;
+    private Long propertyId;
+    private Status status;
+    private String note;
+    private ApplicantDto applicant;
+}
---
+++ src/test/java/org/rent/circle/application/api/dto/ApplicationDtoTest.java
@@ -0,0 +1,21 @@
+package org.rent.circle.application.api.dto;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.force66.beantester.BeanTester;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class ApplicationDtoTest {
+
+    @Test
+    public void ApplicationDto_SettersAndGetters_ShouldWork() {
+        // Arrange
+        BeanTester beanTester = new BeanTester();
+
+        // Act
+        beanTester.testBean(ApplicationDto.class);
+
+        // Assert
+
+    }
+}

be0a5f09d945cc10ea11906310f7ee2a1ab7a96a
matos-nelson <nmatos6@gatech.edu>
2023-08-28 18:58:13 -0500

    Added findApplication method in ApplicationRepository.

--- src/main/java/org/rent/circle/application/api/persistence/repository/ApplicationRepository.java
+++ src/main/java/org/rent/circle/application/api/persistence/repository/ApplicationRepository.java
@@ -2,12 +2,19 @@ package org.rent.circle.application.api.persistence.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Page;
+import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.util.List;
 import org.rent.circle.application.api.persistence.model.Application;
 
 @ApplicationScoped
+
 public class ApplicationRepository implements PanacheRepository<Application> {
+
+    public Application findApplication(Long id, Long managerId) {
+        Parameters queryParams = Parameters.with("id", id).and("managerId", managerId);
+        return find("id = :id and managerId = :managerId", queryParams).firstResult();
+    }
 
     public List<Application> findApplications(Long managerId, int page, int pageSize) {
         return find("managerId", managerId)
